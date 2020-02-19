1521a9a88ce704b08ee1f51e07d272d29540ff06
arlo99 <idv19c.alopez@uartesdigitales.edu.mx>
2020-02-19 11:12:45 -0600

    bleend tree complete and new movement

---
+++ Assets/Scripts/liGameManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class liGameManager : MonoBehaviour
+{
+    [SerializeField]
+    static public float m_time;
+    [SerializeField]
+    static public int m_day;
+    [SerializeField]
+    static public Scene m_scene;
+
+
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    private void Awake()
+    {
+        Instantiate(this);
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
