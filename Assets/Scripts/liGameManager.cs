0aa21e3f2b22ed6d6f77634110546259b380158b
arlo99 <idv19c.alopez@uartesdigitales.edu.mx>
2020-02-26 11:19:20 -0600

    classes para maquina de estados

--- Assets/Scripts/liGameManager.cs
+++ Assets/Scripts/liGameManager.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
+
+
 public class liGameManager : MonoBehaviour
 {
     [SerializeField]
@@ -11,8 +13,16 @@ public class liGameManager : MonoBehaviour
     static public int m_day;
     [SerializeField]
     static public Scene m_scene;
-
-
+    [SerializeField]
+    public Vector3 m_torsoColor;
+    [SerializeField]
+    public Vector3 m_featColor;
+    [SerializeField]
+    public Vector3 m_headColor;
+    [SerializeField]
+    public Vector3 m_skinColor;
+    [SerializeField]
+    public Vector3 m_armColor;
     
     // Start is called before the first frame update
     void Start()
