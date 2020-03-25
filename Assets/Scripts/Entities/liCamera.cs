b651708bd1ff2ee950be2e1cd92a795a8642c55e
Simeone0647 <idv19c.jambrocio@uartesdigitales.edu.mx>
2020-03-25 15:05:18 -0600

    Movimiento de la cámara cuando el personaje está cerca.

--- Assets/Scripts/Entities/liCamera.cs
+++ Assets/Scripts/Entities/liCamera.cs
@@ -8,20 +8,32 @@ public class liCamera : MonoBehaviour
     [SerializeField]
     private float m_positionZ;
 
+    float distance;
+    public float speed = 3;
+    private float offsetZ;
     private void Start()
     {
-        m_positionZ = -10.0f;  
-
+        m_positionZ = -10.0f;
+        
         if(null == m_player)
         {
             m_player = FindObjectOfType<liPlayerCharacter>().gameObject;
-        } 
+        }
+        offsetZ = transform.position.z - m_player.transform.position.z;
     }
 
     void LateUpdate()
     {
-        transform.position = new Vector3(m_player.transform.position.x, 
-                                         m_player.transform.position.y,
-                                         m_positionZ);
+        distance = (transform.position - m_player.transform.position).magnitude;
+        if (distance > 9.2)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, m_player.transform.position
+                + Vector3.forward * offsetZ, speed * Time.fixedDeltaTime);
+           // transform.position = Vector3.Lerp(transform.position,
+           //     m_player.transform.position + Vector3.forward * offsetZ, speed * Time.fixedDeltaTime);
+          //  transform.position = new Vector3(m_player.transform.position.x,
+          //                                   m_player.transform.position.y,
+          //                                   m_positionZ);
+        }
     }
 }
