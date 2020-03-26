91d5095c76615ec293e1b8f836736d5abd36972b
Simeone0647 <idv19c.jambrocio@uartesdigitales.edu.mx>
2020-03-25 19:18:23 -0600

    Mapa de la plaza con colliders al rededor y la cámara funcional.

--- Assets/Scripts/Entities/liCamera.cs
+++ Assets/Scripts/Entities/liCamera.cs
@@ -8,7 +8,8 @@ public class liCamera : MonoBehaviour
     [SerializeField]
     private float m_positionZ;
 
-    float distance;
+    float distance_x;
+    float distance_y;
     public float speed = 3;
     private float offsetZ;
     private void Start()
@@ -24,16 +25,12 @@ public class liCamera : MonoBehaviour
 
     void LateUpdate()
     {
-        distance = (transform.position - m_player.transform.position).magnitude;
-        if (distance > 9.2)
+        distance_x = Mathf.Abs((transform.position.x - m_player.transform.position.x));
+        distance_y = Mathf.Abs((transform.position.y - m_player.transform.position.y));
+        if (distance_x > 2 || distance_y > 1)
         {
-            transform.position = Vector3.MoveTowards(transform.position, m_player.transform.position
-                + Vector3.forward * offsetZ, speed * Time.fixedDeltaTime);
-           // transform.position = Vector3.Lerp(transform.position,
-           //     m_player.transform.position + Vector3.forward * offsetZ, speed * Time.fixedDeltaTime);
-          //  transform.position = new Vector3(m_player.transform.position.x,
-          //                                   m_player.transform.position.y,
-          //                                   m_positionZ);
+           transform.position = Vector3.Lerp(transform.position,
+               m_player.transform.position + Vector3.forward * offsetZ, Time.fixedDeltaTime);
         }
     }
 }
