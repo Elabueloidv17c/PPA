2e78c8771c44ea778efe80a2d48d20bc611a9301
BioAbnerUAD <idv17a.aramirez@uartesdigitales.edu.mx>
2020-04-02 13:26:02 -0600

    camera clamp to map size

--- Assets/Scripts/Entities/liCamera.cs
+++ Assets/Scripts/Entities/liCamera.cs
@@ -13,14 +13,16 @@ public class liCamera : MonoBehaviour
     float minDistance_y = 1;
 
     [SerializeField, DraggablePoint]
-    Vector2 maxCoord;
+    Vector2 maxMapCoord;
     
     [SerializeField, DraggablePoint]
-    Vector2 minCoord;
+    Vector2 minMapCoord;
 
     [SerializeField]
     public float speed = 3;
     private float offsetZ;
+
+    Camera camComp;
     
     private void Start()
     {   
@@ -28,19 +30,35 @@ public class liCamera : MonoBehaviour
         {
             m_target = FindObjectOfType<liPlayerCharacter>().transform;
         }
-        offsetZ = transform.position.z - m_target.transform.position.z;
+        offsetZ = transform.position.z;
+
+        camComp = GetComponent<Camera>();
     }
 
     void LateUpdate()
     {
+        Vector2 camRectSize;
+        camRectSize.x = camComp.orthographicSize * camComp.aspect;
+        camRectSize.y = camComp.orthographicSize;
+
+        Vector2 maxCoord = maxMapCoord.MaxComp(minMapCoord) - camRectSize;
+        Vector2 minCoord = minMapCoord.MinComp(maxMapCoord) + camRectSize;
+
         Vector3 targetPos = ((Vector2)m_target.position).MinComp(maxCoord).MaxComp(minCoord);
+        targetPos.z = offsetZ;
 
         float distance_x = Mathf.Abs((transform.position.x - targetPos.x));
         float distance_y = Mathf.Abs((transform.position.y - targetPos.y));
+
         if (distance_x > minDistance_x || distance_y > minDistance_y)
         {
            transform.position = Vector3.Lerp(transform.position,
-               targetPos + Vector3.forward * offsetZ, Time.fixedDeltaTime);
+                                             targetPos, speed * Time.fixedDeltaTime);
         }
+
+        Vector3 tfrmPos = ((Vector2)transform.position).MinComp(maxCoord).MaxComp(minCoord);
+        tfrmPos.z = offsetZ;
+
+        transform.position = tfrmPos;
     }
 }
